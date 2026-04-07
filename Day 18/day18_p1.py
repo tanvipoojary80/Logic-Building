fd9dedf57719d244211f71520e95104beac16b18
Your Name <you@email.com>
2026-04-07 22:57:41 +0530

    Added Day 18 programs

---
+++ Day 18/day18_p1.py
@@ -0,0 +1,42 @@
+# Pattern - 1
+# *                 *
+# * *             * *
+# * * *         * * *
+# * * * *     * * * *
+# * * * * * * * * * *
+# * * * *     * * * *
+# * * *         * * *
+# * *             * *
+# *                 *
+
+n = 5  # size
+
+# Top half
+for i in range(1, n + 1):
+    # left stars
+    print("* " * i, end="")
+    # middle spaces
+    print("  " * (2 * (n - i)), end="")
+    # right stars
+    print("* " * i)
+    
+# Bottom half
+for i in range(n - 1, 0, -1):
+    # left stars
+    print("* " * i, end="")
+    # middle spaces
+    print("  " * (2 * (n - i)), end="")
+    # right stars
+    print("* " * i)
+
+
+# output:
+# *                 *
+# * *             * *
+# * * *         * * *
+# * * * *     * * * *
+# * * * * * * * * * *
+# * * * *     * * * *
+# * * *         * * *
+# * *             * *
+# *                 *
